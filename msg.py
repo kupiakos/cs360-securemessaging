8ea9e7322ad778c88ead46e8fad04fc69250dbf2
kupiakos <kupiakos@gmail.com>
2016-12-08 17:03:26 -0700

    Add basic crypto

--- msg.py
+++ msg.py
@@ -1,6 +1,11 @@
 import argparse
+import base64
 import socket
 import sys
+import traceback
+
+from Crypto import Random
+from Crypto.PublicKey import RSA
 
 from commands import CommandRunner
 
@@ -8,27 +13,42 @@ from commands import CommandRunner
 class Client(CommandRunner):
     connection = None
     conn_file = None
+    key = None
 
     def run(self, port=8081):
+
         self.connection = socket.create_connection(('localhost', port))
         self.conn_file = self.connection.makefile()
         while True:
             line = ''
             try:
                 line = input('% ')
-            except KeyboardInterrupt:
-                pass
-            except EOFError:
+            except EOFError or KeyboardInterrupt:
                 self.cmd_quit()
             if not line.strip():
                 continue
             try:
                 self.run_command(line)
             except Exception:
-                # traceback.print_exc()
+                traceback.print_exc()
                 print('Error with command')
 
+    def _get_key(self, user: str):
+        self.connection.sendall(b'get_key %s\n' % user.encode())
+        response = self.conn_file.readline()
+        if not response.startswith('key'):
+            return None
+        length = int(response.split()[1])
+        key = self.conn_file.read(length)
+        return key
+
     def cmd_send(self, user: str, subject: str):
+        key_text = self._get_key(user)
+        if key_text is None:
+            print('The user', user, 'does not exist.')
+            return
+        print('key: ', key_text)
+        public_key = RSA.importKey(key_text)
         print('- Type your message. End with a blank line -')
         lines = []
         while True:
@@ -37,8 +57,10 @@ class Client(CommandRunner):
                 break
             lines.append(line)
         message = '\n'.join(lines).encode()
+        encrypted = public_key.encrypt(message, 0)[0]
+        encrypted = base64.b64encode(encrypted)
         self.connection.send(b'put %s %s %d\n%s' % (
-            user.encode(), subject.encode(), len(message), message
+            user.encode(), subject.encode(), len(encrypted), encrypted
         ))
         response = self.conn_file.readline()
         if not response.startswith('OK'):
@@ -54,17 +76,45 @@ class Client(CommandRunner):
         for _ in range(num):
             print(self.conn_file.readline(), end='')
 
-    def cmd_read(self, user: str, index: int):
+    def _read(self, user: str, index: int):
         self.connection.send(b'get %s %d\n' % (user.encode(), index))
         response = self.conn_file.readline()
         if not response.startswith('message'):
-            print(response)
-            return
+            print('server error:', response)
+            return None, None
         _, subject, length = response.strip().split()
         length = int(length)
         message = self.conn_file.read(length)
+        return subject, message
+
+    def cmd_read(self, user: str, index: int):
+        if self.key is None:
+            print('You have not logged in')
+            return
+        subject, encrypted = self._read(user, index)
+        if subject is None:
+            return None
+        encrypted = base64.b64decode(encrypted)
+        message = self.key.decrypt(encrypted).decode()
         print(subject)
         print(message)
+
+    def cmd_peek(self, user: str, index: int):
+        subject, message = self._read(user, index)
+        print(subject)
+        print(message)
+
+    def cmd_login(self, user: str):
+        random_generator = Random.new().read
+        self.key = RSA.generate(2048, random_generator)
+        public_key = self.key.publickey()
+        export = public_key.exportKey()
+        self.connection.sendall(b'store_key %s %d\n%s' % (
+            user.encode(), len(export), export
+        ))
+        response = self.conn_file.readline()
+        if not response.startswith('OK'):
+            print(response)
 
     def cmd_quit(self):
         print()
